230110f73afa20d887080e698e457ab17bf12de8
MARLON SCOTT <link.ms1@gmail.com>
2023-10-02 22:05:38 -0400

    Clean-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Board from './components/Board';
-// import Home from './pages/home';
 
 
 function App() {
   return (
     <div className="App">
-     {/* <Home /> */}
      <Board />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import Board from './components/Board';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <Board /> */}
    <App />
   </React.StrictMode>
 );
